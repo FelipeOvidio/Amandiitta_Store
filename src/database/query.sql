57ce6fd78bf27fb221ddb6cf5a28e48e78715646
FelipeOvidio <f.costaovidio@gmail.com>
2023-12-05 21:15:46 -0300

    add: Alteração de tabelas

--- src/database/query.sql
+++ src/database/query.sql
@@ -5,7 +5,7 @@ create table usuarios
 (
   id serial primary key,
   nome text not null,
-  email text not null,
+  email text not null unique ,
   senha text not null
 );
 
@@ -13,7 +13,9 @@ create table clientes
 (
   id serial primary key,
   nome text not null,
-  telefone text not null
+  email text null unique,
+  endereco text not null, 
+  telefone text not null unique
 );
 
 create table produtos
@@ -23,3 +25,12 @@ create table produtos
   quantidade_estoque integer not null,
   valor numeric not null
 );
+
+create table estoque 
+(
+  id serial primary key,
+  descricao text not null,
+  qtd_estoque int not null,
+  valor numeric not null,
+  
+)
